ea629ead5a0d1c8688feba22bdcbc391095ac7ff
Miguel Prada <mapra99@gmail.com>
2020-10-08 18:35:00 -0500

    cors policy concern added

---
+++ app/controllers/concerns/cors_policy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module CORSPolicy
+  ALLOWED_DOMAIN = ''
+
+  def allow_origins everything: false, local_port: nil
+    if everything
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    elsif local_port.present?
+      response.headers['Access-Control-Allow-Origin'] = "http://localhost:#{local_port}"
+    else
+      response.headers['Access-Control-Allow-Origin'] = ALLOWED_DOMAIN
+    end
+  end
+end
