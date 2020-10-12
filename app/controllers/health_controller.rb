0066a685a5cfbf1d404ce9937fda3100678ac517
Miguel Prada <mapra99@gmail.com>
2020-10-12 16:21:17 -0500

    removed cors policy concern

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,10 +1,4 @@
 class HealthController < ApplicationController
-  include CORSPolicy
-
-  before_action do
-    allow_cross_origins everything: true
-  end
-
   def index
     render json: { status: 'OK' }, status: :ok
   end
